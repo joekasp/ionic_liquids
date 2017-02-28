efe2380a95ff46ffa5d2a26a2a3acebbe062a2d0
Sarah Floris <sdf94@uw.edu>
2017-02-28 15:39:06 -0800

    I created a python code file

---
+++ ionic_liquids/molecular_predictors.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from __future__ import print_function
+from rdkit import Chem
+from rdkit.Chem import AllChem
+from rdkit.Chem import Descriptors
+from rdkit.ML.Descriptors.MoleculeDescriptors import MolecularDescriptorCalculator as Calculator
+
+#molecular descriptors 
+
+
+
+
+#Neural network
+
+
+
+
+
