2daf51b3928da178f2c0b6a9aea82e3e517e063f
Joe Kasper <jkasper2@uw.edu>
2017-03-01 09:59:33 -0800

    Clean directories and make visualization dir

---
+++ ionic_liquids/visualization/core.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+
+def scatter_plot(X,Y):
+    """
+    Draws a scatter plot of data
+
+    Inputs
+    ------
+    X : Input vector of predictor(s) and parameters
+    Y : Prediction of interest
+
+    Returns
+    -------
+    out : Matplotlib axes object containing plot
+
+    """
+    pass
+
