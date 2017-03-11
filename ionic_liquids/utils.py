83d16f88afb5000590a04a9e76083865ac36d8b3
Joe Kasper <joe.kasper@gmail.com>
2017-03-11 09:27:45 -0800

    Need someone to put ML algorithms in functions

--- ionic_liquids/utils.py
+++ ionic_liquids/utils.py
@@ -6,18 +6,39 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.neural_network import MLPRegressor
 from sklearn.svm import SVR
 from sklearn.model_selection import GridSearchCV
-from sklearn.linear_model import LassoCV
+from sklearn.linear_model import Lasso
 
 def save_model(obj,model_type,filename='default'):
     items = []
     if (model_type == 'lasso'):
-        pass
+        items.append(obj.coef_)
+        items.append(obj.sparse_coef_)
+        items.append(obj.intercept_)
+        items.append(obj.n_iter_)
     elif (model_type == 'mlp_reg'):
-        pass
+        items.append(obj.loss_)
+        items.append(obj.coefs_)
+        items.append(obj.intercepts_)
+        items.append(obj.n_iter_)
+        items.append(obj.n_layers_)
+        items.append(obj.n_outputs_)
+        items.append(obj.out_activation_)
     elif (model_type == 'mlp_clas'):
-        pass
+        items.append(obj.classes_)
+        items.append(obj.loss_)
+        items.append(obj.coefs_)
+        items.append(obj.intercepts_)
+        items.append(obj.n_iter_)
+        items.append(obj.n_layers_)
+        items.append(obj.n_outputs_)
+        items.append(obj.out_activation_)
     elif (model_type == 'svr'):
-        pass
+        items.append(obj.support_)
+        items.append(obj.support_vectors_)
+        items.append(obj.dual_coef_)
+        items.append(obj.coef_)
+        items.append(obj.intercept_)
+        items.append(obj.sample_weight_)
     else:
         Raise ValueError('Invalid model type!')
 
