82b00cf141921788c6a7687913ec145f8408ad94
Sarah Floris <sdf94@uw.edu>
2017-03-11 12:14:40 -0800

    I am creating the line to the main function that can be run

--- ionic_liquids/front_functions/data_generate.py
+++ ionic_liquids/front_functions/data_generate.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import numpy as np
 
-def data_cleaning(dataframe):
+def data_cleaning(filename):
 
 	#Data Cleaning
 	data = pd.read_excel("inputdata.xlsx")
 	data['EC_value'], data['EC_error'] = zip(*data['ELE_COD'].map(lambda x: x.split('±')))
-
+	
 	return data
--- ionic_liquids/main.py
+++ ionic_liquids/main.py
@@ -3,11 +3,18 @@ import pandas as pd
 from front_functions import * 
 import utils
 
+
 #get X matrix and response vector y (need a function for this) 
-X,y = gen_data(filename) 
+data_frame = data_generate(filename) 
+
+#calculate molecular descriptors
+molecular_descriptors, conductivity = molecular_descriptors(data_frame)
 
 #do machine_learning call
-obj = MLP_regressor(X,y)
+MLP_Regr = MLP_regressor(molecular_descriptors, conductivity)
+MLP_class = MLP_classifier(molecular_descriptors, conductivity)
+Lasso = Lasso(molecular_descriptors, conductivity)
+SVR = SVR(molecular_descriptors, conductivity)
 
 #save model to file
 utils.save_model(obj,model_type='mlp_reg')
