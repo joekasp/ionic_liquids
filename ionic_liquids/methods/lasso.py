a61362d848f151e1cf2ce96c2fee165dc0b579d2
Sarah Floris <sdf94@uw.edu>
2017-03-11 21:53:47 -0800

    I added a errors.py

--- ionic_liquids/methods/lasso.py
+++ ionic_liquids/methods/lasso.py
@@ -21,9 +21,9 @@ def do_lasso(X,y):
     alphas = np.array([5,4,3,2,1,0.1,0.01,0.001,0.0001])
     lasso = Lasso(alpha=0.001, fit_intercept=True, normalize=False, precompute=False,
         copy_X=True, max_iter=10000, tol=0.001, positive=False, random_state=None, selection='cyclic')
-    #gs = GridSearchCV(lasso, param_grid=dict(alpha=alphas))
-    #gs.fit(X,y)
-    lasso.fit(X,y)
+    gs = GridSearchCV(lasso, param_grid=dict(alpha=alphas))
+    gs.fit(X,y)
+    #lasso.fit(X,y)
 	
     return lasso
 
