d1b15203f74da79fe47f79e57ef35e1dab3830be
Hongbin Liu <hbliu@uw.edu>
2017-03-12 23:37:29 -0700

    Add dot string for all functions, add comments where necessary

--- ionic_liquids/utils.py
+++ ionic_liquids/utils.py
@@ -1,3 +1,4 @@
+#import packages
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
@@ -11,37 +12,72 @@ from rdkit import Chem
 from rdkit.Chem import AllChem, Descriptors
 from sklearn.model_selection import train_test_split
 from rdkit.ML.Descriptors.MoleculeDescriptors import MolecularDescriptorCalculator as Calculator
-
 from methods import methods
 
+
 def errors(y_test,y_prediction):
-		difference = y_prediction - y_test
-		return difference
+	'''Generate the prediction error'''
+	difference = y_prediction - y_test
+
+	return difference
 
 
 def train_model(model,data_file,save=True):
-    df = read_data(data_file)
-    X,y = molecular_descriptors(df)
-    print("training model is ",model)
-    if (model == 'LASSO'):
-        obj = lasso.do_lasso(X,y)
-    elif (model == 'MLP Regressor'):
-        pass
-    elif (model == 'MLP Classifier'):
-        pass
-    elif (model == 'SVR'):
-        pass
-    else:
-        raise ValueError('Invalid model type!') 
+	'''
+	Choose the regression model
+	
+	Input
+	------
+	model: string, the model to use
+	data_file: dataframe, cleaned csv data
+	
+	Returns
+	------
+	obj: objective, the regressor
+	X: dataframe, normlized input feature
+	y: targeted electrical conductivity
 
-    return obj, X, y
+	'''
+
+	df = read_data(data_file)
+	X,y = molecular_descriptors(df)
+	print("training model is ",model)
+	if (model == 'LASSO'):
+	    obj = lasso.do_lasso(X,y)
+	elif (model == 'MLP Regressor'):
+	    pass
+	elif (model == 'MLP Classifier'):
+	    pass
+	elif (model == 'SVR'):
+	    pass
+	else:
+	    raise ValueError('Invalid model type!') 
+
+	return obj, X, y
 
 
 def molecular_descriptors(data):
-	#Setting up for molecular descriptors
+	'''
+	Use RDKit to prepare the molecular descriptor
+	
+	Inputs
+	------
+	data: dataframe, cleaned csv data
+
+	Returns
+	------
+	norm_X: normalized input features
+	Y: experimental electrical conductivity
+
+	'''
+
 	n = data.shape[0]
-	list_of_descriptors = ['NumHeteroatoms', 'ExactMolWt', 'NOCount', 'NumHDonors',
-		'RingCount', 'NumAromaticRings', 'NumSaturatedRings','NumAliphaticRings']
+	# Choose which molecular descriptor we want
+	list_of_descriptors = ['NumHeteroatoms', 'ExactMolWt',
+				'NOCount', 'NumHDonors',
+				'RingCount', 'NumAromaticRings', 
+				'NumSaturatedRings','NumAliphaticRings']
+	# Get the molecular descriptors and their dimension
 	calc = Calculator(list_of_descriptors)
 	D = len(list_of_descriptors)
 	d = len(list_of_descriptors)*2 + 4
@@ -58,12 +94,21 @@ def molecular_descriptors(data):
 		X[i][:D]    = calc.CalcDescriptors(A)
 		X[i][D:2*D] = calc.CalcDescriptors(B)
 
-	new_data = pd.DataFrame(X,columns=['NUM', 'NumHeteroatoms_A', 'MolWt_A', 'NOCount_A',
-		'NumHDonors_A', 'RingCount_A', 'NumAromaticRings_A', 'NumSaturatedRings_A',
-		'NumAliphaticRings_A', 'NumHeteroatoms_B', 'MolWt_B', 'NOCount_B', 'NumHDonors_B',
-		'RingCount_B', 'NumAromaticRings_B', 'NumSaturatedRings_B', 'NumAliphaticRings_B',
-		'T', 'P', 'MOLFRC_A'])
-	return new_data, Y
+	prenorm_X = pd.DataFrame(X,columns=['NUM', 'NumHeteroatoms_A', 
+				'MolWt_A', 'NOCount_A','NumHDonors_A', 
+				'RingCount_A', 'NumAromaticRings_A', 
+				'NumSaturatedRings_A',
+				'NumAliphaticRings_A', 
+				'NumHeteroatoms_B', 'MolWt_B', 
+				'NOCount_B', 'NumHDonors_B',
+				'RingCount_B', 'NumAromaticRings_B', 
+				'NumSaturatedRings_B', 
+				'NumAliphaticRings_B',
+				'T', 'P', 'MOLFRC_A'])
+	# Normalize all the input feature
+	norm_X = StandardScaler().fit_transform(prenorm_X)
+
+	return norm_X, Y
 
 
 def read_data(filename):
@@ -96,62 +141,90 @@ def read_data(filename):
 
 
 def save_model(obj,model_type,filename='default'):
-    items = []
-    if (model_type == 'lasso'):
-        items.append(obj.coef_)
-        items.append(obj.sparse_coef_)
-        items.append(obj.intercept_)
-        items.append(obj.n_iter_)
-    elif (model_type == 'mlp_reg'):
-        items.append(obj.loss_)
-        items.append(obj.coefs_)
-        items.append(obj.intercepts_)
-        items.append(obj.n_iter_)
-        items.append(obj.n_layers_)
-        items.append(obj.n_outputs_)
-        items.append(obj.out_activation_)
-    elif (model_type == 'mlp_clas'):
-        items.append(obj.classes_)
-        items.append(obj.loss_)
-        items.append(obj.coefs_)
-        items.append(obj.intercepts_)
-        items.append(obj.n_iter_)
-        items.append(obj.n_layers_)
-        items.append(obj.n_outputs_)
-        items.append(obj.out_activation_)
-    elif (model_type == 'svr'):
-        items.append(obj.support_)
-        items.append(obj.support_vectors_)
-        items.append(obj.dual_coef_)
-        items.append(obj.coef_)
-        items.append(obj.intercept_)
-        items.append(obj.sample_weight_)
-    else:
-        raise ValueError('Invalid model type!')
+	'''
+	Save the trained regressor model to the file
 
-    if (filename == 'default'):
-        filename = 'model_' + model_type + '.txt'
+	Input
+	------
+	obj: objective, the regressor objective
+	model_type: string, the name of the model
 
-    f = open(filename,'w')
-    f.write(model_type + '\n')
-    for item in items:
-        f.write(str(item))
-        f.write('\n')
-    f.close()
- 
-    return
+	Returns
+	------
+	None
+	
+	'''
+
+	items = []
+	if (model_type == 'lasso'):
+	    items.append(obj.coef_)
+	    items.append(obj.sparse_coef_)
+	    items.append(obj.intercept_)
+	    items.append(obj.n_iter_)
+	elif (model_type == 'mlp_reg'):
+	    items.append(obj.loss_)
+	    items.append(obj.coefs_)
+	    items.append(obj.intercepts_)
+	    items.append(obj.n_iter_)
+	    items.append(obj.n_layers_)
+	    items.append(obj.n_outputs_)
+	    items.append(obj.out_activation_)
+	elif (model_type == 'mlp_clas'):
+	    items.append(obj.classes_)
+	    items.append(obj.loss_)
+	    items.append(obj.coefs_)
+	    items.append(obj.intercepts_)
+	    items.append(obj.n_iter_)
+	    items.append(obj.n_layers_)
+	    items.append(obj.n_outputs_)
+	    items.append(obj.out_activation_)
+	elif (model_type == 'svr'):
+	    items.append(obj.support_)
+	    items.append(obj.support_vectors_)
+	    items.append(obj.dual_coef_)
+	    items.append(obj.coef_)
+	    items.append(obj.intercept_)
+	    items.append(obj.sample_weight_)
+	else:
+	    raise ValueError('Invalid model type!')
+	
+	if (filename == 'default'):
+	    filename = 'model_' + model_type + '.txt'
+	
+	f = open(filename,'w')
+	f.write(model_type + '\n')
+	for item in items:
+	    f.write(str(item))
+	    f.write('\n')
+	f.close()
+	
+	return
 
 
 def read_model(filename,model_type):
-    if (model_type == 'lasso'):
-        obj = Lasso
-    elif (model_type == 'mlp_reg'):
-        obj = MLPRegressor
-    elif (model_type == 'mlp_clas'):
-        obj = MLPClassifier
-    elif (model_type == 'svr'):
-        obj = SVR
-    else:
-        raise ValueError('Invalid model type!')
-    return obj
+	'''
+	Read the trained regressor to 
+	avoid repeating training.
+
+	Input
+	------
+	model_type: string, the name of the 
+			regression model
+	
+	Returns
+	------
+	obj: objective, the regressor objective
+	
+	'''
+	if (model_type == 'lasso'):
+	    obj = Lasso
+	elif (model_type == 'mlp_reg'):
+	    obj = MLPRegressor
+	elif (model_type == 'mlp_clas'):
+	    obj = MLPClassifier
+	elif (model_type == 'svr'):
+	    obj = SVR
+	else:
+	    raise ValueError('Invalid model type!')
+	return obj
 
