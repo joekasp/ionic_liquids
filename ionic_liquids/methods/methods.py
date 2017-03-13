65c636bd03505706b94af1acb5e88df74552e594
Sarah Floris <sdf94@uw.edu>
2017-03-13 14:46:14 -0700

    Changed alpha values

--- ionic_liquids/methods/methods.py
+++ ionic_liquids/methods/methods.py
@@ -97,7 +97,7 @@ def do_lasso(X,y):
 
 	"""
 
-	alphas = np.array([5,4,3,2,1,0.1,0.01,0.001,0.0001])
+	alphas = np.array([0.1,0.01,0.001,0.0001])
 	lasso = Lasso(alpha=0.001, fit_intercept=True, normalize=False, precompute=False,
 	    copy_X=True, max_iter=10000, tol=0.001, positive=False, random_state=None, selection='cyclic')
 	#gs = GridSearchCV(lasso, param_grid=dict(alpha=alphas))
