6dc6ef2fe6719a778a729b482253e2de6870453d
Joe Kasper <jkasper2@uw.edu>
2017-03-14 11:09:01 -0700

    Able to read SMILES and compounds for prediction

--- ionic_liquids/visualization/core.py
+++ ionic_liquids/visualization/core.py
@@ -6,6 +6,42 @@ def model_types():
     models=['MLP Regressor','MLP Classifier','LASSO','SVR']
     return models
 
+def read_SMILES():
+    """
+    Reads compound names and SMILES from file
+
+    Inputs
+    -----
+    None
+
+
+    Returns
+    ------
+    A_list : names of 'A' compounds
+    A_smiles : SMILES of corresponding 'A' compounds
+    B_list : names of 'B' compounds
+    B_smiles : SMILES of corresponding 'B' compounds
+
+    """
+
+    df = pd.read_excel('datasets/compoundSMILES.xlsx')
+    A_list = []
+    A_smiles = []
+    B_list = []
+    B_smiles = []
+
+    #remove duplicates
+    for row in df.itertuples():
+        if row[1] not in A_list:
+            A_list.append(row[1])
+            A_smiles.append(row[2])
+        if row[3] not in B_list:
+            B_list.append(row[3])
+            B_smiles.append(row[4])
+        
+
+    return A_list, A_smiles, B_list, B_smiles
+
 def scatter_plot(X,Y):
     """
     Draws a scatter plot of data
