9a98fee924bff60e89727fbdce2e95262386fee6
Joe Kasper <jkasper2@uw.edu>
2017-03-15 14:08:04 -0700

    Main routine should work. Cleaning up code to pep8

--- ionic_liquids/main.py
+++ ionic_liquids/main.py
@@ -3,37 +3,39 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 
-import utils 
+import utils
 from methods import methods
 from visualization import plots
 
 
-
 FILENAME = 'datasets/inputdata.xlsx'
-MODEL = 'lasso'
+MODEL = 'mlp_regressor'
+DIRNAME = 'my_test'
 
-#get X matrix and response vector y (need a function for this) 
-df = utils.read_data(FILENAME) 
+# get X matrix and response vector y (need a function for this)
+df = utils.read_data(FILENAME)
 X, y = utils.molecular_descriptors(df)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
 
-#do machine_learning call
+# normalize testing data using training data
+X_train, X_mean, X_std = utils.normalization(X_train)
+X_test, trash, trash = utils.normalization(X_test, X_mean, X_std)
 
-if (MODEL.lower() == 'mlp_regressor'): 
+# do machine_learning call
+if (MODEL.lower() == 'mlp_regressor'):
     obj = methods.do_MLP_regressor(X_train, y_train)
-elif (MODEL.lower() == 'mlp_classifier'): 
+elif (MODEL.lower() == 'mlp_classifier'):
     obj = methods.do_MLP_classifier(X_train, y_train)
-elif (MODEL.lower() == 'lasso'): 
-    obj = methods.do_lasso(X_train,y_train)
-elif (MODEL.lower() == 'svr'): 
-    obj = methods.do_svr(X_train,y_train)
+elif (MODEL.lower() == 'lasso'):
+    obj = methods.do_lasso(X_train, y_train)
+elif (MODEL.lower() == 'svr'):
+    obj = methods.do_svr(X_train, y_train)
 else:
     raise ValueError("Model not supported")
 
-#save model to file
-utils.save_model(obj,model_type=MODEL)
+# save model to file
+utils.save_model(obj, X_mean, X_std, X_train, y_train, dirname=DIRNAME)
 
-#plot
-my_plot = plots.parity_plot(y,obj.predict(X))
+# plot
+my_plot = plots.parity_plot(y_train, obj.predict(X_train))
 plt.show(my_plot)
-
