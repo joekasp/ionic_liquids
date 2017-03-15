d8a9eae7be480512529957f860cd6c245b591588
Joe Kasper <jkasper2@uw.edu>
2017-03-15 13:29:22 -0700

    working on cleaning up code to pep8

--- ionic_liquids/utils.py
+++ ionic_liquids/utils.py
@@ -1,7 +1,7 @@
-#default python modules
+# default python modules
 import os
 from datetime import datetime
-#external packages
+# external packages
 import numpy as np
 import pandas as pd
 from sklearn.externals import joblib
@@ -13,56 +13,52 @@ from sklearn.neural_network import MLPRegressor
 from sklearn.svm import SVR
 from rdkit import Chem
 from rdkit.Chem import AllChem, Descriptors
-from rdkit.ML.Descriptors.MoleculeDescriptors import MolecularDescriptorCalculator as Calculator
-#internal modules
+from rdkit.ML.Descriptors.MoleculeDescriptors \
+    import MolecularDescriptorCalculator as Calculator
+# internal modules
 from methods import methods
 
 
-def errors(y_test,y_prediction):
-    '''Generate the prediction error'''
-    difference = y_prediction - y_test
-    return difference
-
-
-def train_model(model,data_file,test_percent,save=True):
+def train_model(model, data_file, test_percent, save=True):
     """
     Choose the regression model
-    
+
     Input
     ------
     model: string, the model to use
     data_file: dataframe, cleaned csv data
     test_percent: float, the percentage of data held for testing
-    
+
     Returns
     ------
     obj: objective, the regressor
     X: dataframe, normlized input feature
     y: targeted electrical conductivity
-    
-    """    
+
+    """
     df = read_data(data_file)
-    X,y = molecular_descriptors(df)
-    X_train, X_test, y_train, y_test  = train_test_split(X,y,test_size=(test_percent/100))
+    X, y = molecular_descriptors(df)
+    X_train, X_test, y_train, y_test = \
+        train_test_split(X, y, test_size=(test_percent/100))
     X_train, X_mean, X_std = normalization(X_train)
-    
-    model = model.replace(' ','_')
-    #print("training model is ",model)
+
+    model = model.replace(' ', '_')
+    # print("training model is ",model)
     if (model.lower() == 'lasso'):
-        obj = methods.do_lasso(X_train,y_train)
+        obj = methods.do_lasso(X_train, y_train)
     elif (model.lower() == 'mlp_regressor'):
-        obj = methods.do_MLP_regressor(X_train,y_train)
+        obj = methods.do_MLP_regressor(X_train, y_train)
     elif (model.lower() == 'mlp_classifier'):
-        obj = methods.do_MLP_classifier(X_train,y_train)
+        obj = methods.do_MLP_classifier(X_train, y_train)
     elif (model.lower() == 'svr'):
-        obj = methods.do_svr(X_train,y_train)
+        obj = methods.do_svr(X_train, y_train)
     else:
-        raise ValueError('Invalid model type!') 
-    
+        raise ValueError('Invalid model type!')
+
     return obj, X_train, y_train, X_mean, X_std
 
 
-def normalization(data,means=None,stdevs=None):
+def normalization(data, means=None, stdevs=None):
     """
     Normalizes the data using the means and standard
     deviations given, calculating them otherwise.
@@ -82,32 +78,29 @@ def normalization(data,means=None,stdevs=None):
 
     """
     cols = data.columns
-    data = data.values   
-   
+    data = data.values
+
     if (means is None) or (stdevs is None):
-        means = np.mean(data,axis=0)
-        stdevs = np.std(data,axis=0,ddof=1)
+        means = np.mean(data, axis=0)
+        stdevs = np.std(data, axis=0, ddof=1)
     else:
         means = np.array(means)
         stdevs = np.array(stdevs)
-  
-    #print('Data Shape:',data.shape)
-    #print('Means Shape:',means.shape)
-    #print('StDev Shape:',stdevs.shape)
-    
-    #handle special case of one row
+
+    # handle special case of one row
     if (len(data.shape) == 1) or (data.shape[0] == 1):
         for i in range(len(data)):
             data[i] = (data[i] - means[i]) / stdevs[i]
     else: 
         for i in range(data.shape[1]):
             data[:,i] = (data[:,i] - means[i]*np.ones(data.shape[0])) / stdevs[i]
-    
-    normed = pd.DataFrame(data,columns=cols)
-    return normed, means, stdevs
-    
 
-def predict_model(A_smile,B_smile,obj,t,p,m,X_mean,X_stdev,flag=None):
+    normed = pd.DataFrame(data, columns=cols)
+
+    return normed, means, stdevs
+
+
+def predict_model(A_smile, B_smile, obj, t, p, m, X_mean, X_stdev, flag=None):
     """
     Generates the predicted model data for a mixture
     of compounds A and B at temperature t and pressure p.
@@ -130,64 +123,66 @@ def predict_model(A_smile,B_smile,obj,t,p,m,X_mean,X_stdev,flag=None):
     y_pred : predicted conductivity (y_values)
 
     """
-    N = 100 #number of points
+    N = 100 # number of points
 
     y_pred = np.empty(N+1)
     if (flag == 'm'):
-        x_conc = np.linspace(0,1,N+1)
+        x_conc = np.linspace(0, 1, N+1)
     elif (flag == 't'):
-        x_conc = np.linspace(100,400,N+1)
+        x_conc = np.linspace(100, 400, N+1)
     elif (flag == 'p'):
-        x_conc = np.linspace(5,400,N+1)
+        x_conc = np.linspace(5, 400, N+1)
     else:
         raise ValueError("unexpected flag")
     for i in range(len(x_conc)):
         if (flag == 'm'):
-            my_df = pd.DataFrame({'A':A_smile,'B':B_smile,'MOLFRC_A':x_conc[i],'P':p,'T':t,'EC_value':0},index=[0])
+            my_df = pd.DataFrame({'A': A_smile, 'B': B_smile, 'MOLFRC_A': x_conc[i], \
+                                'P': p, 'T': t, 'EC_value': 0}, index=[0])
         elif (flag == 't'):
-            my_df = pd.DataFrame({'A':A_smile,'B':B_smile,'MOLFRC_A':m,'P':p,'T':x_conc[i],'EC_value':0},index=[0])
+            my_df = pd.DataFrame({'A': A_smile, 'B': B_smile, 'MOLFRC_A': m, \
+                                 'P': p, 'T': x_conc[i], 'EC_value': 0}, index=[0])
         elif (flag == 'p'):
-            my_df = pd.DataFrame({'A':A_smile,'B':B_smile,'MOLFRC_A':m,'P':x_conc[i],'T':t,'EC_value':0},index=[0])
-            
-        X,trash = molecular_descriptors(my_df)
-        X,trash,trash = normalization(X,X_mean,X_stdev)
+            my_df = pd.DataFrame({'A': A_smile, 'B': B_smile, 'MOLFRC_A': m, \
+                                 'P': x_conc[i], 'T': t, 'EC_value': 0}, index=[0])
+        X, trash = molecular_descriptors(my_df)
+        X, trash, trash = normalization(X, X_mean, X_stdev)
         y_pred[i] = obj.predict(X)
-    
-    return x_conc,y_pred
-   
+
+
+    return x_conc, y_pred
 
 
 def molecular_descriptors(data):
     """
     Use RDKit to prepare the molecular descriptor
-    
+
     Inputs
     ------
     data: dataframe, cleaned csv data
-    
+
     Returns
     ------
     prenorm_X: normalized input features
     Y: experimental electrical conductivity
-    
+
     """
-    
+
     n = data.shape[0]
     # Choose which molecular descriptor we want
     list_of_descriptors = ['NumHeteroatoms', 'ExactMolWt',
         'NOCount', 'NumHDonors',
         'RingCount', 'NumAromaticRings', 
-        'NumSaturatedRings','NumAliphaticRings']
+        'NumSaturatedRings', 'NumAliphaticRings']
     # Get the molecular descriptors and their dimension
     calc = Calculator(list_of_descriptors)
     D = len(list_of_descriptors)
     d = len(list_of_descriptors)*2 + 4
-    
+
     Y = data['EC_value']
-    X = np.zeros((n,d))
-    X[:,-3] = data['T']
-    X[:,-2] = data['P']
-    X[:,-1] = data['MOLFRC_A']
+    X = np.zeros((n, d))
+    X[:, -3] = data['T']
+    X[:, -2] = data['P']
+    X[:, -1] = data['MOLFRC_A']
     for i in range(n):
         A = Chem.MolFromSmiles(data['A'][i])
         B = Chem.MolFromSmiles(data['B'][i])
@@ -205,9 +200,9 @@ def molecular_descriptors(data):
         'NumSaturatedRings_B', 
         'NumAliphaticRings_B',
         'T', 'P', 'MOLFRC_A'])
-    
-    prenorm_X = prenorm_X.drop('NumAliphaticRings_A',1) 
-    prenorm_X = prenorm_X.drop('NumAliphaticRings_B',1)   
+
+    prenorm_X = prenorm_X.drop('NumAliphaticRings_A', 1)
+    prenorm_X = prenorm_X.drop('NumAliphaticRings_B', 1)
 
     return prenorm_X, Y
 
@@ -221,7 +216,7 @@ def read_data(filename):
     filename : string of path to file
 
     Returns
-    ------ 
+    ------
     df : Pandas DataFrame
 
     """
@@ -230,23 +225,23 @@ def read_data(filename):
     filetype = cols[1]
     if (filetype == 'csv'):
         df = pd.read_csv(filename)
-    elif (filetype in ['xls','xlsx']):
+    elif (filetype in ['xls', 'xlsx']):
         df = pd.read_excel(filename)
     else:
         raise ValueError('Filetype not supported')
 
-    #clean the data if necessary
+    # clean the data if necessary
     df['EC_value'], df['EC_error'] = zip(*df['ELE_COD'].map(lambda x: x.split('±')))
-    df = df.drop('EC_error',1)
-    df = df.drop('ELE_COD',1)
+    df = df.drop('EC_error', 1)
+    df = df.drop('ELE_COD', 1)
  
     return df
 
 
-def save_model(obj,X_mean,X_stdev,X=None,y=None,dirname='default'):
+def save_model(obj, X_mean, X_stdev, X=None, y=None, dirname='default'):
     """
     Save the trained regressor model to the file
-    
+
     Input
     ------
     obj: model object
@@ -254,35 +249,35 @@ def save_model(obj,X_mean,X_stdev,X=None,y=None,dirname='default'):
     X_stdev : stdev for each column of training X
     X : Predictor matrix
     y : Response vector
-    dirname : the directory to save contents  
- 
+    dirname : the directory to save contents
+
     Returns
     ------
     None
     """
     if (dirname == 'default'):
         timestamp = str(datetime.now())[:19]
-        dirname = 'model_'+timestamp.replace(' ','_')
+        dirname = 'model_'+timestamp.replace(' ', '_')
     else:
         pass
     if not os.path.exists(dirname):
         os.makedirs(dirname)
-        
-    filename = dirname + '/model.pkl'   
-    joblib.dump(obj,filename)  
 
-    joblib.dump(X_mean,dirname+'/X_mean.pkl')
-    joblib.dump(X_stdev,dirname+'/X_stdev.pkl')
+    filename = dirname + '/model.pkl'
+    joblib.dump(obj, filename)
+
+    joblib.dump(X_mean, dirname+'/X_mean.pkl')
+    joblib.dump(X_stdev, dirname+'/X_stdev.pkl')
 
     if (X is not None):
         filename = dirname + '/X_data.pkl'
-        joblib.dump(X,filename)
+        joblib.dump(X, filename)
     else:
         pass
 
     if (y is not None):
         filename = dirname + '/y_data.pkl'
-        joblib.dump(y,filename)
+        joblib.dump(y, filename)
     else:
         pass
 
@@ -291,13 +286,13 @@ def save_model(obj,X_mean,X_stdev,X=None,y=None,dirname='default'):
 
 def read_model(file_dir):
     """
-    Read the trained regressor to 
+    Read the trained regressor to
     avoid repeating training.
-    
+
     Input
     ------
     file_dir : the directory containing all model info
-    
+
     Returns
     ------
     obj: model object
@@ -305,10 +300,10 @@ def read_model(file_dir):
     X_stdev : stdev of columns in training X
     X : predictor matrix (if it exists) otherwise None
     y : response vector (if it exists) otherwise None
-    
+
     """
     filename = file_dir + '/model.pkl'
-    obj = joblib.load(filename) 
+    obj = joblib.load(filename)
     X_mean = joblib.load(file_dir+'/X_mean.pkl')
     X_stdev = joblib.load(file_dir+'/X_stdev.pkl')
 
@@ -320,6 +315,5 @@ def read_model(file_dir):
         y = joblib.load(file_dir + '/y_data.pkl')
     except:
         y = None
-    
-    return obj, X_mean, X_stdev, X, y
 
+    return obj, X_mean, X_stdev, X, y
